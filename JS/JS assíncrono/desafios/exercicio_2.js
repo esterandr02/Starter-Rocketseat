3ec72cdf6982e1c532d2db4c079394ef952060a8
esterandr02 <esterandr02@gmail.com>
2020-03-18 23:05:26 -0300

    corrigindo o desafio 3 (busca no github)

--- JS/JS assíncrono/desafios/exercicio_2.js
+++ JS/JS assíncrono/desafios/exercicio_2.js
@@ -2,12 +2,21 @@ var userName = document.querySelector('#user input');
 var btn = document.querySelector('#user button');
 var list = document.querySelector('#user ul');
 
-// esterandr02 pjavier98
+function createLoading() {
+    var loading = document.createElement('li');
+    loading.appendChild(document.createTextNode('Loading...'));
+    list.appendChild(loading);
+
+    return loading;
+}
 
 btn.onclick = function() {
+    var load = createLoading();
+    
     axios.get('https://api.github.com/users/'+ userName.value + '')
-
-        .then(function(response) {
+    
+    .then(function(response) {
+        list.removeChild(load);
 
         // LOGIN
             var login = document.createElement('li');
@@ -17,12 +26,14 @@ btn.onclick = function() {
         // AVATAR URL
             var linkAvatar = document.createElement('a');
             linkAvatar.setAttribute('href', response.data.avatar_url);
-            linkAvatar.appendChild(document.createTextNode('link do avatar'));
+            linkAvatar.appendChild(document.createTextNode('avatar link'));
             list.appendChild(linkAvatar);
         })
 
         .catch(function(error) {
+            list.removeChild(load);
             console.warn(error);
+            alert('ERROR 404! user does not exist. Please reload the page, and try again');
         });
 
 // REPOSITORIOS
@@ -32,19 +43,24 @@ btn.onclick = function() {
         .then(function(response) {
 
         // LISTA COM REPOSITORIOS
+
             var repoList = document.createElement('ol');
-            repoList.appendChild(document.createTextNode('### Repositories ###'));
+            repoList.appendChild(document.createTextNode('******* Repositories *******'));
             
             for (repo of response.data) {
-                var repositoryName = document.createElement('li');
+                var item = document.createElement('li');
+                var repositoryName = document.createElement('a');
+
+                repositoryName.setAttribute('href', 'https://github.com/' + userName.value + '/' + repo.name + '');
                 repositoryName.appendChild(document.createTextNode(repo.name));
-                repoList.appendChild(repositoryName);
+                
+                item.appendChild(repositoryName);
+                repoList.appendChild(item);
             }
+            
             list.appendChild(repoList);
         })
         .catch(function(error) {
             console.warn(error);
         });
-
-    userName.value = '';
 }
